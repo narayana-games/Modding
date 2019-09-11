bcdd87d6fae32cb5fc01250962c002df983e14c1
Jashan Chittesh <jashan@narayana-games.net>
2019-09-11 21:48:22 +0200

    Better data layout for PostProcessingMod

--- com.narayana-games.modding/Runtime/PostProcessingMod.cs
+++ com.narayana-games.modding/Runtime/PostProcessingMod.cs
@@ -40,44 +40,56 @@ namespace NarayanaGames.BeatTheRhythm.Modding {
         [Header("General")]
         public float transitionTime = 0.3F;
 
-        // see https://github.com/Unity-Technologies/PostProcessing/wiki/Bloom
-        // for how all of these work
-
-        [Header("Bloom")]
-        public float intensity = 1F;
-        public float threshold = 0.8F;
-        // maybe add Clamp?
-        [Range(0, 1)]
-        public float softKnee = 0.5F;
-        [Range(0, 5)]
-        public float diffusion = 5F;
-        
-        // see https://github.com/Unity-Technologies/PostProcessing/wiki/Color-Grading
-        // for how all of these work
-        
         [Space]
-        [Header("White Balance")]
-        
-        [Range(-100, 100)]
-        public float temperature = 0F;
-        
-        [Range(-100, 100)]
-        public float tint = 0F;
-        
-        
-        [Header("Tone")]
-        public float exposure = 0.5F;
-        public Color colorFilter = Color.white;
-        
-        [Range(-100, 100)]
-        public float hueShift = 0F;
-     
-        [Range(-100, 100)]
-        public float saturation = 0F;
+        public Bloom bloom = new Bloom();
 
-        [Range(-100, 100)]
-        public float contrast = 0F;
+        [Serializable]
+        public class Bloom {
+            // see https://github.com/Unity-Technologies/PostProcessing/wiki/Bloom
+            // for how all of these work
+            
+            public float intensity = 1F;
+
+            public float threshold = 0.8F;
+
+            // maybe add Clamp?
+            [Range(0, 1)]
+            public float softKnee = 0.5F;
+
+            [Range(0, 5)]
+            public float diffusion = 5F;
+        }
+
+        [Space]
+        public ColorGrading colorGrading = new ColorGrading();
         
+        [Serializable]
+        public class ColorGrading {
+            // see https://github.com/Unity-Technologies/PostProcessing/wiki/Color-Grading
+            // for how all of these work
+
+            [Header("White Balance")]
+
+            [Range(-100, 100)]
+            public float temperature = 0F;
+
+            [Range(-100, 100)]
+            public float tint = 0F;
+
+
+            [Header("Tone")]
+            public float exposure = 0.5F;
+
+            public Color colorFilter = Color.white;
+
+            [Range(-100, 100)]
+            public float hueShift = 0F;
+
+            [Range(-100, 100)]
+            public float saturation = 0F;
+
+            [Range(-100, 100)]
+            public float contrast = 0F;
+        }
     }
-
 }
